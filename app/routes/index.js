fa3de2937a23ec15706508e54d8c645120f5fb61
Andrei Georgescu <ageorgescu98@gmail.com>
2021-05-31 11:58:17 -0700

    initial commit

---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Andrei Georgescu | Software Engineer' });
+});
+
+router.get('/blog', (req, res) => {
+  res.redirect('https://medium.com/@ageor');
+});
+
+module.exports = router;
