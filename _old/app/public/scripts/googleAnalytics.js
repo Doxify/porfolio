007b9571ec0b6057b5d01fa4e2c9f0cbc62c449c
Andrei Georgescu <ageorgescu98@gmail.com>
2021-08-09 21:53:08 -0700

    Delete _old directory

--- _old/app/public/scripts/googleAnalytics.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prefer-rest-params */
-/* eslint-disable no-undef */
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-BWJZB4JQX8');
